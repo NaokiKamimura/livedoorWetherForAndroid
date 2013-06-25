7da8bc8147c1c8d4a60da7a7208e4099c0e3fe28
Naoki Kamimura <n.kamimura1988@gmail.com>
2013-06-25 15:22:10 +0900

    commit

--- WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/wethernewsforlivedoor/SecondActivity.java
+++ WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/wethernewsforlivedoor/SecondActivity.java
@@ -1,8 +1,13 @@
 package jp.gr.java_conf.naoki_kamimura.wethernewsforlivedoor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
 
@@ -10,6 +15,31 @@ public class SecondActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_second);
+
+		// 画面表示リスト
+		ListView list = (ListView) findViewById(R.id.listview);
+		// ダミーデータ
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+		for (int i = 0; i < 20; i++) {
+			adapter.add("Apple");
+		}
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				ListView list = (ListView) parent;
+				String msg = "index:" + position;
+				makeToast(msg);
+				//タッチされた場所のアイテムを取得する
+				String item = (String) list.getItemAtPosition(position);
+				makeToast(item);
+
+			}
+		});
+		//ダミーデータ終わり
+
 	}
 
 	@Override
@@ -17,6 +47,11 @@ public class SecondActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.second, menu);
 		return true;
+	}
+
+	public void makeToast(String message) {
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT)
+				.show();
 	}
 
 }
