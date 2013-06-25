5afaf7bfcb2090b1c20864d0fd87ee5a5aade72f
Naoki Kamimura <n.kamimura1988@gmail.com>
2013-06-25 11:41:29 +0900

    commit

---
+++ WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/util/Http.java
@@ -0,0 +1,6 @@
+package jp.gr.java_conf.naoki_kamimura.util;
+
+public class Http {
+	
+
+}
---
+++ WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/wethernewsforlivedoor/MainActivity.java
@@ -0,0 +1,64 @@
+package jp.gr.java_conf.naoki_kamimura.wethernewsforlivedoor;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button button = (Button) findViewById(R.id.main_button);
+		//表示するボタンを押すとSecondActivityへ移動する
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+		makeToast("test");
+		makeDialog("タイトル", "本文");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * @version 1.00 24 June 2013
+	 * @author N.Kamimura
+	 * @param 通知する文字列
+	 *            トースト通知を表示
+	 */
+	public void makeToast(String message) {
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT)
+				.show();
+	}
+
+	/**
+	 * @version 1.00 24 June 2013
+	 * @author N.Kamimura
+	 * @param タイトル
+	 *            ,本文 アラートダイアログを表示
+	 */
+	public void makeDialog(String title, String message) {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+		dialog.setTitle(title);
+		dialog.setMessage(message);
+		dialog.setPositiveButton("OK", null);
+		dialog.show();
+	}
+
+}
