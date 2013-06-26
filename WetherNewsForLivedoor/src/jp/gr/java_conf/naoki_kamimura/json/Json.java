3342f5a2c2b08792f1ec0b4489d25695a381def1
Naoki Kamimura <n.kamimura1988@gmail.com>
2013-06-26 11:57:04 +0900

    commit

--- WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/json/Json.java
+++ WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/json/Json.java
@@ -16,43 +16,52 @@ import com.google.gson.Gson;
 
 public class Json {
 
-	// Gsonライブラリを使い解析テスト用にまずJSON形式で生成する
+	/**
+	 * @version 1.00 25 June 2013
+	 * @author NaokiKamimura
+	 * Gsonライブラリを使い解析テスト用にまずJSON形式で生成する
+	 */
+	// 
 	public void createJson() {
 		LogUtil log = new LogUtil();
 		// Gsonのオブジェクト作成
 		Gson gson = new Gson();
 		String testJson = gson.toJson("test");
-		log.putLog("testJson", testJson);
+		log.output("testJson", testJson);
 	}
 
-	// JSONの解析を行う
+	/**
+	 * @version 1.00 25 June 2013
+	 * @author NaokiKamimura
+	 * JSONファイルを解析する
+	 */
 	public void readJson() {
 		LogUtil log = new LogUtil();
 		V1 v1;
 		try {
 			Gson gson = new Gson();
-			log.putLog("logFlag", "0");
+			log.output("logFlag", "0");
 			// TODO ここから先が読み込まれていないので、パスの指定がおかしい気がする
-			FileReader reader = new FileReader("jsonfile/v1.json");
-			log.putLog("logFlag", "1");
+			FileReader reader = new FileReader("/jsonfile/v1.json");
+			log.output("logFlag", "1");
 			v1 = gson.fromJson(reader, V1.class);// JSON
 			reader.close();
 			List<PinpointLocations> pinpointLocations = v1
 					.getPinpointLocations();
 			String jsonLog = "";
 			String jsonSize = String.valueOf(pinpointLocations.size());
-			log.putLog("logFlag", "2");
+			log.output("logFlag", "2");
 			for (int i = 0; i < pinpointLocations.size(); i++) {
 				jsonLog = pinpointLocations.get(i).toString();
-				log.putLog("logFlag", "3");
-				log.putLog("jsonLog", jsonSize);
-				log.putLog("jsonLog", jsonLog);
+				log.output("logFlag", "3");
+				log.output("jsonLog", jsonSize);
+				log.output("jsonLog", jsonLog);
 			}
 
 		} catch (FileNotFoundException e) {
-			log.putLog("fileNotFound", "");
+			log.output("fileNotFound", "");
 		} catch (Exception e) {
-			log.putLog("Error", "");
+			log.output("Error", "");
 		}
 	}
 
--- WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/util/LogUtil.java
+++ WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/util/LogUtil.java
@@ -6,8 +6,17 @@ package jp.gr.java_conf.naoki_kamimura.util;
 
 import android.util.Log;
 
+/**
+ * @version 1.00 25 June 2013
+ * @author N.Kamimura
+ */
 public class LogUtil {
-	public void putLog(String tag, String log) {
+	/*
+	 * @author NaokiKamimura
+	 * @param タグ,ログに表示する内容
+	 * タグと内容を送り、ログに表示
+	 */
+	public void output(String tag, String log) {
 		Log.v(tag, log);
 	}
 
