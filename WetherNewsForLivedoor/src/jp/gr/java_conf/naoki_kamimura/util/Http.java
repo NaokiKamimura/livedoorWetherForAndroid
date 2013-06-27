27ee1f9e6295cffcc88b6d6ee9047b7a29a76e26
Naoki Kamimura <n.kamimura1988@gmail.com>
2013-06-27 18:29:47 +0900

    commit

--- WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/util/Http.java
+++ WetherNewsForLivedoor/src/jp/gr/java_conf/naoki_kamimura/util/Http.java
@@ -5,6 +5,13 @@
 
 package jp.gr.java_conf.naoki_kamimura.util;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 public class Http {
@@ -13,8 +20,39 @@ public class Http {
 	 * @version 1.00 27 June 2013
 	 * @author NaokiKamimura HTTP通信を開始する
 	 */
-	public void Connection() {
+	public void connection(String address) {
+		LogUtil log = new LogUtil();
 		DefaultHttpClient httpClient = new DefaultHttpClient();
+		try {
+			URI uri = new URI(address);
+			HttpGet request = new HttpGet(uri);
+			HttpResponse response = httpClient.execute(request);
+		} catch (URISyntaxException e) {
+			log.output("URISyntaxException", "URLとして解析出来ませんでした");
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			log.output("ClientProtocolException", "URLとして解析出来ませんでした");
+			e.printStackTrace();
+		} catch (IOException e) {
+			log.output("IOException", "URLとして解析出来ませんでした");
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @version 1.00 27 June 2013
+	 * @author NaokiKamimura HTTP通信を切断する
+	 */
+	public void disconnection() {
+
+	}
+
+	/**
+	 * @version 1.00 27 June 2013
+	 * @author NaokiKamimura HTTP通信の状態を確認する
+	 */
+	public void conditionCheck() {
 
 	}
 
